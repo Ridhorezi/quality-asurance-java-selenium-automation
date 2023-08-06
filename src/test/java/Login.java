ebab252ba33f11d934710e2b40910a111d99ef87
Ridho Suhaebi Arrowi <ridhorezi1212@gmail.com>
2023-08-06 22:00:16 +0700

    first commit

---
+++ src/test/java/Login.java
@@ -0,0 +1,86 @@
+import org.junit.jupiter.api.Test;
+import config.env_target;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Login extends env_target {
+    @Test
+    public void main() {
+        //Set driver location path
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\drivers\\chromedriver.exe");
+        //Maximize driver
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //Set URL
+        driver.get(baseUrl);
+        //Set duration
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='submit'][@data-test='login-button']"))
+        );
+        driver.findElement(By.name("user-name")).sendKeys("standard_user");
+        driver.findElement(By.name("password")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='title'][contains(text(),'Products')]"))
+        );
+        //Set Quit
+        driver.quit();
+    }
+
+    @Test
+    public void invalidUserName() {
+        //Set driver location path
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\drivers\\chromedriver.exe");
+        //Maximize driver
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //Set URL
+        driver.get(baseUrl);
+        //Set duration
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='submit'][@data-test='login-button']"))
+        );
+        driver.findElement(By.name("user-name")).sendKeys("standard_sauce");
+        driver.findElement(By.name("password")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='title'][contains(text(),'Products')]"))
+        );
+        //Set Quit
+        driver.quit();
+    }
+
+    @Test
+    public void invalidPassword() {
+        //Set driver location path
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\drivers\\chromedriver.exe");
+        //Maximize driver
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //Set URL
+        driver.get(baseUrl);
+        //Set duration
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='submit'][@data-test='login-button']"))
+        );
+        driver.findElement(By.name("user-name")).sendKeys("standard_user");
+        driver.findElement(By.name("password")).sendKeys("secret_user");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='title'][contains(text(),'Products')]"))
+        );
+        //Set Quit
+        driver.quit();
+    }
+}
